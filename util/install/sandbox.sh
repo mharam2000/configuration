acaa7ddbf57e963e00a374bddbacef3dcda82fb7
mallgates <mharam2000@hotmail.com>
2017-06-11 03:41:23 +0200

    Create sandbox.sh

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -11,10 +11,10 @@
 ##
 ## Sanity check
 ##
-if [[ `lsb_release -rs` != "16.04" ]]; then
-   echo "This script is only known to work on Ubuntu 16.04, exiting...";
-   exit;
-fi
+#if [[ `lsb_release -rs` != "16.04" ]]; then
+ #  echo "This script is only known to work on Ubuntu 16.04, exiting...";
+  # exit;
+#fi
 
 ##
 ## Set ppa repository source for gcc/g++ 4.8 in order to install insights properly
